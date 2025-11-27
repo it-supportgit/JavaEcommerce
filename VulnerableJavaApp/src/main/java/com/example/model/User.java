d91472d3d877e6febbc8b041d91d959aa33e1640
Santosh Sattayya Gudla <CON073@rlpl.com>
2025-11-27 13:15:57 +0530

    Java test project

---
+++ VulnerableJavaApp/src/main/java/com/example/model/User.java
@@ -0,0 +1,19 @@
+package com.example.model;
+public class User implements java.io.Serializable {
+    private int id;
+    private String username;
+    private String passwordHash;
+    private boolean adminFlag = false;
+    public User() {}
+    public User(int id, String username, String passwordHash) {
+        this.id = id; this.username = username; this.passwordHash = passwordHash;
+    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+    public boolean isAdminFlag() { return adminFlag; }
+    public void setAdminFlag(boolean adminFlag) { this.adminFlag = adminFlag; }
+}
